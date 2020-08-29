97ddaeba272e93e09597308d45b281b654bdedc3
Ashley Berry <ashleywberry@gmail.com>
2020-08-29 11:58:43 -0500

    rounded to two decimals

--- scripts/client.js
+++ scripts/client.js
@@ -65,11 +65,12 @@ function calculateTotalCosts(){
     //calculate total monthly costs
     for (let i = 0; i < employees.length; i++){
         //for each employee salary, add up total of all costs
-        monthlyTotal += Math.round((Number(employees[i].annualSalary)) / 12);
+        monthlyTotal += ((Number(employees[i].annualSalary)) / 12);
     } // end for
     let el = $( '#totalMonthly' );
     el.empty();
-    el.append(monthlyTotal);
+    el.append((monthlyTotal).toFixed(2));
+    //twoDecimals();
     checkTotal();
 } // end calculateTotalCosts
 
@@ -79,30 +80,6 @@ function checkTotal(){
         $('#totalMonthly').addClass('red');
     }
 } // end checkTotal
-
-function emptyInputs(){
-    console.log('in emptyInputs')
-    $('#firstNameIn').val('');
-    $('#lastNameIn').val('');
-    $('#idIn').val('');
-    $('#jobTitleIn').val('');
-    $('#annualSalaryIn').val('');
-}
-
-function onReady(){
-    // capture click event on element with "addEmployeeButton"
-    $( '#addEmployeeButton' ).on( 'click', addEmployee );
-    $(document).on('click', '.removeEmployeeBtn', onRemove);
-    displayEmployees();
-}
-
-function onRemove(){
-    console.log('in onRemove')
-    $(this).closest('tr').remove();
-    //target monthly total
-    //subtract this employee's salary
-};
-
 
 function displayEmployees(){
     console.log( 'in displayEmployees' );
@@ -123,4 +100,27 @@ function displayEmployees(){
         ` );
     } // end for
     calculateTotalCosts();
-} // end displayInventory
+} // end displayEmployees
+
+function emptyInputs(){
+    console.log('in emptyInputs')
+    $('#firstNameIn').val('');
+    $('#lastNameIn').val('');
+    $('#idIn').val('');
+    $('#jobTitleIn').val('');
+    $('#annualSalaryIn').val('');
+} // end emptyInputs
+
+function onReady(){
+    // capture click event on element with "addEmployeeButton"
+    $( '#addEmployeeButton' ).on( 'click', addEmployee );
+    $(document).on('click', '.removeEmployeeBtn', onRemove);
+    displayEmployees();
+} // end onReady
+
+function onRemove(){
+    console.log('in onRemove')
+    $(this).closest('tr').remove();
+    //target monthly total
+    //subtract this employee's salary
+} // end onRemove
